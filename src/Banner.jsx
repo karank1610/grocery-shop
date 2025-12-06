48d665047669feaaee892338f74b770d0de598cd
karank1610 <karank16102003@gmail.com>
2025-12-06 18:37:35 +0530

    banner added

---
+++ src/Banner.jsx
@@ -0,0 +1,22 @@
+const Banner = () => {
+    return (
+        <>
+            <div className="banner-main">
+                <div className="static-banner-1">
+                    <div className="banner-img">
+                        <a href=""><img src="assets/img/banner-1.jpg" alt="banner" /></a>
+                    </div>
+                    <div className="banner-text">
+                        <p> <span>Flat <strong>20</strong>% Off <br /> On Grocery </span><br /> Min. $200 Purchase. <br /> Save Today!!</p>
+                    </div>
+                    <div className="banner-shop-button">
+                        <p><a href="">shop today</a></p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Banner;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Banner from "./Banner";
 
 const Home = () => {
     return (
         <>
         <Header/>
+        <Banner/>
         </>
     )
 }
