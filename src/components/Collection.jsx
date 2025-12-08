c560ea2c6dd3f433fecab41cb76fecbc16fd70e9
karank1610 <karank16102003@gmail.com>
2025-12-08 13:19:06 +0530

    collection new and on sale text added

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -26,14 +26,14 @@ const Collection = () => {
                             </div>
                         </div>
                     </div> */}
-                    <CollectionProps img={'assets/img/collection1.jpg'} name={'perspiciatis unde'} price={'$99.00'}/>
-                    <CollectionProps img={'assets/img/collection2.jpg'} name={'omnis iste'} price={'$199.00'}/>
-                    <CollectionProps img={'assets/img/collection3.jpg'} name={'accusantium dolore'} price={'$299.00'}/>
-                    <CollectionProps img={'assets/img/collection4.jpg'} name={'rem aperiam'} price={'$249.00'}/>
-                    <CollectionProps img={'assets/img/collection5.jpg'} name={'adipisci velit'} price={'$269.00'}/>
-                    <CollectionProps img={'assets/img/collection6.jpg'} name={'numquam eius'} price={'$169.00'}/>
-                    <CollectionProps img={'assets/img/collection7.jpg'} name={'modi tempora'} price={'$79.00'}/>
-                    <CollectionProps img={'assets/img/collection8.jpg'} name={'dolore magnam'} price={'$229.00'}/>
+                    <CollectionProps img={'assets/img/collection1.jpg'} name={'perspiciatis unde'} price={'$99.00'} isNew={true} isOnSale={true}/>
+                    <CollectionProps img={'assets/img/collection2.jpg'} name={'omnis iste'} price={'$199.00'} isNew={true} isOnSale={false}/>
+                    <CollectionProps img={'assets/img/collection3.jpg'} name={'accusantium dolore'} price={'$299.00'} isNew={true} isOnSale={true}/>
+                    <CollectionProps img={'assets/img/collection4.jpg'} name={'rem aperiam'} price={'$249.00'} isNew={false} isOnSale={true}/>
+                    <CollectionProps img={'assets/img/collection5.jpg'} name={'adipisci velit'} price={'$269.00'} isNew={true} isOnSale={true}/>
+                    <CollectionProps img={'assets/img/collection6.jpg'} name={'numquam eius'} price={'$169.00'} isNew={true} isOnSale={false}/>
+                    <CollectionProps img={'assets/img/collection7.jpg'} name={'modi tempora'} price={'$79.00'} isNew={true} isOnSale={true}/>
+                    <CollectionProps img={'assets/img/collection8.jpg'} name={'dolore magnam'} price={'$229.00'} isNew={false} isOnSale={false}/>
                 </div>
             </div>
         </div>
--- src/props/CollectionProps..jsx
+++ src/props/CollectionProps..jsx
@@ -3,18 +3,20 @@ import React from "react";
 const CollectionProps = (props) => {
     return (
         <>
-        <div className="collection-child">
-                        <div className="collection-img">
-                            <a href=""><img src={props.img} alt="collection" /></a>
-                        </div>
-                        <div className="collection-details">
-                            <a href=""><span className="collection-name">{props.name}</span></a>
-                            <span className="collection-price">{props.price}</span>
-                            <div className="collection-shop-btn">
-                                <p><a href="">add to cart</a></p>
-                            </div>
-                        </div>
+            <div className="collection-child">
+                <div className="collection-img">
+                    <a href=""><img src={props.img} alt="collection" /></a>
+                </div>
+                {props.isNew && <div className="collection-new-text">New</div>}
+                {props.isOnSale && <div className="collection-sale-text">on Sale!</div>}
+                <div className="collection-details">
+                    <a href=""><span className="collection-name">{props.name}</span></a>
+                    <span className="collection-price">{props.price}</span>
+                    <div className="collection-shop-btn">
+                        <p><a href="">add to cart</a></p>
                     </div>
+                </div>
+            </div>
         </>
     )
 }
