490a9fbaa8f81af7642cbbb22ba1fea226bf06cf
karank1610 <karank16102003@gmail.com>
2026-03-21 15:55:08 +0530

    warning fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,13 +7,13 @@ const Banner = () => {
                 <IoIosArrowBack className="banner-arrow-left" />
                 <div className="static-banner-1">
                     <div className="banner-img">
-                        <a href=""><img src="assets/img/banner-1.jpg" alt="banner" /></a>
+                        <a href="/"><img src="assets/img/banner-1.jpg" alt="banner" /></a>
                     </div>
                     <div className="banner-text">
                         <p> <span>Flat <strong>20</strong>% Off <br /> On Grocery </span><br /> Min. $200 Purchase. <br /> Save Today!!</p>
                     </div>
                     <div className="banner-shop-button">
-                        <p><a href="">shop today</a></p>
+                        <p><a href="/">shop today</a></p>
                     </div>
                 </div>
                 <IoIosArrowForward className="banner-arrow-right" />
