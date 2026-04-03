a06ab7154c60d542687637161cd2e25dd6aab9e9
karank1610 <karank16102003@gmail.com>
2026-04-03 18:33:29 +0530

    banner slider added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,26 +1,82 @@
-import { IoIosArrowBack,IoIosArrowForward } from "react-icons/io";
+import { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+const banners = [
+    {
+        id: 1,
+        img: "/assets/img/banner-1.jpg",
+        label: "On Grocery",
+        highlight: "20",
+        prefix: "Flat",
+        suffix: "% Off",
+        sub: "Min. $200 Purchase. Save Today!!",
+        btnText: "shop today",
+    },
+    {
+        id: 2,
+        img: "/assets/img/banner-1.jpg",
+        label: "On Fashion",
+        highlight: "30",
+        prefix: "Upto",
+        suffix: "% Off",
+        sub: "Min. $150 Purchase. Limited Time!!",
+        btnText: "shop now",
+    },
+    {
+        id: 3,
+        img: "/assets/img/banner-1.jpg",
+        label: "On Fashion",
+        highlight: "40",
+        prefix: "Upto",
+        suffix: "% Off",
+        sub: "Min. $150 Purchase. Limited Time!!",
+        btnText: "shop now",
+    },
+];
 
 const Banner = () => {
-    return (
-        <>
-            <div className="banner-main">
-                <IoIosArrowBack className="banner-arrow-left" />
-                <div className="static-banner-1">
-                    <div className="banner-img">
-                        <a href="/"><img src="assets/img/banner-1.jpg" alt="banner" /></a>
-                    </div>
-                    <div className="banner-text">
-                        <p> <span>Flat <strong>20</strong>% Off <br /> On Grocery </span><br /> Min. $200 Purchase. <br /> Save Today!!</p>
-                    </div>
-                    <div className="banner-shop-button">
-                        <p><a href="/">shop today</a></p>
-                    </div>
-                </div>
-                <IoIosArrowForward className="banner-arrow-right" />
-            </div>
-        </>
-    )
+    const [current, setCurrent] = useState(0);
 
-}
+    const prev = () => setCurrent(i => (i === 0 ? banners.length - 1 : i - 1));
+    const next = () => setCurrent(i => (i === banners.length - 1 ? 0 : i + 1));
+
+    const banner = banners[current];
+
+    return (
+        <div className="banner-main">
+            <IoIosArrowBack className="banner-arrow-left" onClick={prev} />
+
+            <div className="static-banner-1">
+                <div className="banner-img">
+                    <a href="/"><img src={banner.img} alt="banner" /></a>
+                </div>
+                <div className="banner-text">
+                    <p>
+                        <span>
+                            {banner.prefix} <strong>{banner.highlight}</strong>{banner.suffix}
+                            <br />{banner.label}
+                        </span>
+                        <br />{banner.sub}
+                    </p>
+                </div>
+                <div className="banner-shop-button">
+                    <p><a href="/">{banner.btnText}</a></p>
+                </div>
+            </div>
+
+            <IoIosArrowForward className="banner-arrow-right" onClick={next} />
+
+            <div className="banner-dots">
+                {banners.map((_, i) => (
+                    <span
+                        key={i}
+                        className={`banner-dot ${i === current ? "active" : ""}`}
+                        onClick={() => setCurrent(i)}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Banner;
